8645a6015bfc80141704254e8b0adf4224e26977
Michael Meier <mmeier61@yahoo.com>
2016-04-07 13:51:36 -0500

    brought in

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,6 +9,10 @@ class UserController < ApplicationController
     @user = User.find_by_id(params[:id])
     @assigned = Assignment.where("user_id" => @user.id)
     
+    user = current_user
+    @assignments = user.assignments
+    @task = Task.all
+    @family = User.where("family_id" => user.family_id)
 
  
     render 'view'
